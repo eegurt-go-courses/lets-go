86df25b8b72c9e59f6e59c009c0f5bbe25443a30
eegurt <alihan28.06.0450@gmail.com>
2022-10-29 16:47:54 +0600

    html validation errors, validator

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,8 +6,10 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/julienschmidt/httprouter"
 	"snippetbox.eegurt.net/internal/models"
+	"snippetbox.eegurt.net/internal/validator"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -48,8 +50,19 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	app.render(w, http.StatusOK, "view.html", data)
 }
 
+type snippetCreateForm struct {
+	Title   string
+	Content string
+	Expires int
+	validator.Validator
+}
+
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
+
+	data.Form = snippetCreateForm{
+		Expires: 365,
+	}
 
 	app.render(w, http.StatusOK, "create.html", data)
 }
@@ -60,15 +73,30 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		app.clientError(w, http.StatusBadRequest)
 	}
 
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
-
 	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 	}
 
-	id, err := app.snippets.Insert(r.Context(), title, content, expires)
+	form := snippetCreateForm{
+		Title:   r.PostForm.Get("title"),
+		Content: r.PostForm.Get("content"),
+		Expires: expires,
+	}
+
+	form.CheckField(validator.NotBlank(form.Title), "title", "This filed cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This filed cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This filed cannot be blank")
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
+
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "create.html", data)
+		return
+	}
+
+	id, err := app.snippets.Insert(r.Context(), form.Title, form.Content, form.Expires)
 	if err != nil {
 		app.serverError(w, err)
 		return
