fb527ef46f77770349aae9955fd5bbec63827810
eegurt <alihan28.06.0450@gmail.com>
2022-11-11 18:32:13 +0600

    user signup

--- go.mod
+++ go.mod
@@ -20,6 +20,6 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.12.0 // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
-	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/crypto v0.2.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
 )
--- internal/models/users.go
+++ internal/models/users.go
@@ -1,9 +1,14 @@
 package models
 
 import (
+	"context"
+	"errors"
+	"strings"
 	"time"
 
+	"github.com/jackc/pgconn"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -18,7 +23,25 @@ type UserModel struct {
 	DB *pgxpool.Pool
 }
 
-func (m *UserModel) Insert(name, email, password string) error {
+func (m *UserModel) Insert(ctx context.Context, name, email, password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	query := `INSERT INTO users (name, email, hashed_password, created)
+	VALUES($1, $2, $3, NOW())`
+
+	_, err = m.DB.Exec(ctx, query, name, email, string(hashedPassword))
+	if err != nil {
+		var postgresError *pgconn.PgError
+		if errors.As(err, &postgresError) {
+			if postgresError.Code == "23505" && strings.Contains(postgresError.Message, "users_uc_email") {
+				return ErrDuplicateEmail
+			}
+		}
+		return err
+	}
 	return nil
 }
 
